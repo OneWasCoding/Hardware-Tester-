1239a45458efd4875b7f45cd0f9551110da955f2
S1ngularty <asherxd10245@gmail.com>
2025-04-05 15:48:17 +0800

    item update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ItemsController;
 
 
@@ -23,7 +23,7 @@ Route::get('items/restore/{id}', [ItemsController::class, 'restore'])->name('ite
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('customer/cart')->group(function () {
     Route::get('/', [CartController::class, 'index'])->name('cart.index')->middleware('auth');
