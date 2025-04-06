29acfb707e1d7fedbbe43b69f77af596e0d4618b
S1ngularty <asherxd10245@gmail.com>
2025-04-06 15:11:45 +0800

    excel imports

--- resources/views/admin/item/index.blade.php
+++ resources/views/admin/item/index.blade.php
@@ -11,7 +11,21 @@
     }
 </style>
 @section('content')
+
 <div class="content">
+    <div class="excel_import">
+        <form action="{{ route('item.import') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <label for="file">Select Excel File:</label>
+                <input type="file" name="file" required>
+            </div>
+        
+            <br>
+        
+            <button type="submit">Import Items</button>
+        </form>
+    </div>
     <h2 class="mb-4  " id='user_header' style="color:rgb(100, 100, 224)">Item List</h2>
     
     <div class="card">
