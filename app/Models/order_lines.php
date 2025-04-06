697c6651fb25b927244cb4a761e9c419c16a207c
S1ngularty <asherxd10245@gmail.com>
2025-04-06 16:52:02 +0800

    transaction models

--- app/Models/order_lines.php
+++ app/Models/order_lines.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class order_lines extends Model
 {
-    //
+    protected $table = 'order_lines'; // Name of the table in the database
+    protected $primaryKey = 'order_line_id'; // Primary key for the order_lines table
+    public $timestamps = true; // Enable created_at and updated_at fields
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'price'];
 }
