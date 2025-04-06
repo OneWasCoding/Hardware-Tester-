fa3e51e062d7eaab75b8f0a9024921e11bf171bf
S1ngularty <asherxd10245@gmail.com>
2025-04-06 20:09:08 +0800

    email

--- config/mail.php
+++ config/mail.php
@@ -36,6 +36,16 @@ return [
     */
 
     'mailers' => [
+        'mailtrap' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
+            ],
 
         'smtp' => [
             'transport' => 'smtp',
